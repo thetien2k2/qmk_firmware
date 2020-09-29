d7d9c031b35c4f51320398a5a845de3e572359ec
thetien2k2 <thetien2k2@gmail.com>
2020-09-29 19:01:51 +0700

    update keymap and upstream

--- keyboards/xd75/keymaps/thetien2k2/keymap.c
+++ keyboards/xd75/keymaps/thetien2k2/keymap.c
@@ -31,23 +31,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
     KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,                  KC_MINS, KC_GRV, KC_EQL,    KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_LBRC, KC_BSLS, KC_RBRC,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT,
-    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,                  KC_NO, KC_DEL, KC_NO,       KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-    KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_NO, KC_UP, KC_NO,        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
+    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,                  KC_HOME, KC_DEL, KC_END,       KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+    KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_PGDOWN, KC_UP, KC_PGUP,        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
     KC_LCTL, KC_LGUI, KC_LALT, MO(_S0), KC_SPC, KC_SPC,     KC_LEFT, KC_DOWN, KC_RGHT,  KC_SPC, KC_SPC, MO(_S1), KC_RALT, KC_RGUI, KC_RCTL
   ),
   [_S0] = LAYOUT_ortho_5x15( /* FUNCTION */
     KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,               KC_F11, KC_F12, KC_F13,     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_F14, KC_F15, KC_F16,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_PSCR, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_WH_L, KC_PGUP, KC_WH_R,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, _______, KC_NO, KC_NO,             KC_HOME, KC_PGDN, KC_END,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO,         KC_F14, KC_F15, KC_F16,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_MRWD, KC_MPLY, KC_MFFD, KC_NO, KC_NO, KC_NO,         KC_BTN1, KC_PSCR, KC_BTN2,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_WH_L, KC_MS_U, KC_WH_R,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, _______, KC_NO, KC_NO,             KC_MS_L, KC_MS_D, KC_MS_R,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
   ),
   [_S1] = LAYOUT_ortho_5x15( /* FUNCTION */
     BL_TOGG, BL_BRTG, BL_DEC, BL_INC, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RESET,
     RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUD, RGB_SAD, RGB_SAI,   RGB_VAD, RGB_VAI, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO,         KC_BTN4, KC_BTN3, KC_BTN5,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_MRWD, KC_MPLY, KC_MFFD, KC_NO, KC_NO, KC_NO,         KC_BTN1, KC_MS_U, KC_BTN2,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    CG_TOGG, KC_NO, AG_TOGG, KC_NO, KC_NO, KC_NO,           KC_MS_L, KC_MS_D, KC_MS_R,        KC_NO, KC_NO, _______, KC_NO, KC_NO, KC_NO
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_BTN2,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    CG_TOGG, KC_NO, AG_TOGG, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, _______, KC_NO, KC_NO, KC_NO
   )
 };
 
